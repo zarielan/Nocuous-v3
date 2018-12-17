f03e36d48a127cdee444495dd6584b6730e90799
zarielan <28706022+zarielan@users.noreply.github.com>
2018-12-17 21:16:11 +0800

    Level reading from a file

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
+using System;
 
-public class LevelScript : MonoBehaviour {
+public class LevelScript : MonoBehaviour
+{
+    public string levelFile;
+
+    private string[] levelData;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+    {
+        levelData = readFile(levelFile);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
 		
 	}
+
+    private string[] readFile(string file)
+    {
+        string[] lines = System.IO.File.ReadAllLines("Assets/Levels/" + file);
+        //print(lines);
+
+        return lines;
+    }
 }
